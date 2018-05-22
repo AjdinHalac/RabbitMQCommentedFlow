bcd4b361713a73d26835d255eac71a6624dc3d72
Ajdin Halac <halac.ajdin@nsoft.com>
2018-05-22 14:24:40 +0200

    Add consumer comments

--- PersistentRMQ/consumer.go
+++ PersistentRMQ/consumer.go
@@ -33,7 +33,9 @@ func main() {
 	 * I want to crate a single task queue which is durable and won't lose my message after it stops executing.
 	 * If multiple consumers are started, every N-th consumer will get every N-th message.
 	 * The issue with this solution is in scenario where all odd message have a huge load while all the even messages are lightly processed.
-	 * Solution for this issue is allowing our consumers to read one message at a time.
+	 * There still exists an issue where one working dieing will lose the message it was currently processing, also losing all of the messages dispatched to it.
+	 * To solve this issue, we will acknowledge messages after they have been completely processed to avoid them getting deleted too early.
+	 * Also lets allow our consumers to only read one message at a time, so they get dispatched more evenly.
 	 * Take a look at ch.Qos, prefetch count tells the RMQ server to have one message in the consumer till it gets ack-ed.
 	 */
 	q, err := ch.QueueDeclare(
@@ -52,6 +54,17 @@ func main() {
 		false, // global
 	)
 
+	/*
+	 * Analogue to our Publish method in producer we have a Consume method in consumer which reads from the queue.
+	 * Consume method accepts few parameters:
+	 * - First parameter is a string value holding the name of the queue we are consuming from. In the case below from our freshly declared queue.
+	 * - Second is a consumer tag
+	 * - Third parameter tells the RabbitMQ that we will manually acknowledge messages when we feel they've been processed.
+	 * - Exclusive flag notes that this queue can only be used by the connection that opened it. Exclusive queues are deleted after the connection closes.
+	 * - If the no-local flag is set, the RMQ server will not send messages to the connection that published them.
+	 * - If the no-wait flag is set the server will not respond to the method.
+	 * - Args are additional arguments you find fit for the queue.
+	 */
 	msgs, err := ch.Consume(
 		q.Name, // queue
 		"",     // consumer
@@ -72,7 +85,7 @@ func main() {
 			t := time.Duration(dot_count)
 			time.Sleep(t * time.Second)
 			log.Printf("Done")
-			d.Ack(false)
+			d.Ack(false) // This is worth noting in this block of code. This will ack the messages back to the queue.
 		}
 	}()
 
